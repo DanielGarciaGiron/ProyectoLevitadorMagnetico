96e11dfa6d76e965e0d7496a523880453d74d79d
DanielGarciaGiron <daniel.garcia.giron@gmail.com>
2018-05-17 17:26:07 -0600

    Initial commit

---
+++ LevitadorMagnetico.ino
@@ -0,0 +1,14 @@
+//Universidad del Valle de Guatemala 
+//Proyecto Levitador Magnetico
+//Daniel Garcia, 14152
+//Ricardo Galindo, 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
