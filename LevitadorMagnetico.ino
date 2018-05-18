76e879ef498e68d0d51498610ca7efaf44f8bb44
DanielGarciaGiron <daniel.garcia.giron@gmail.com>
2018-05-18 10:36:27 -0600

    Full Code

--- LevitadorMagnetico.ino
+++ LevitadorMagnetico.ino
@@ -3,46 +3,115 @@
 //Daniel Garcia, 14152
 //Ricardo Galindo, 
 
+//Librerias necesarias
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
 
-
+//Analog Inputs, Potenciometro y sensores de efecto Hall.
 int SensorH = 5;
 int SensorL = 6;
 int Ref = 7;
-int e = 0;
-int ed = 0;
 int Control_Voltage = 0;
-int Kp = 0;
-int Ki = 0;
-int Kd = 0;
-int e_old = 0;
-int E_old = 0;
 int Signal_Ind = 0;
 int Actual_Value = 0;
+int Signal_Mag = 0;
+int Vref = 0;
 
+//Variables de salida
+int controlVoltage = 0; 
+byte DACSignal = B00000000;
+const int lowestBit = 31;
+const int highestBit = 39;
+int currentBit = 0;
 
-void setup() {
-  // put your setup code here, to run once:
+//Variables PID
+uint8_t redLedState = LOW;
+uint8_t tiempo = LOW;
+int erroractual = 0;
+int Erroractual = 0;
+int Errorviejo = 0;
+int errorviejo = 0;
+int errorderivativo = 0;
+int errortotal = 0;
+int Pot = 0;
+int sensorValue = 0;
+int sensorValue1 = 0;
+int Sensores = 0;
   
+int Kd = 0;
+int Kp = 2;
+int Ki = 0;
+
+void setup()
+{
+  //Timer y comunicacion serial
+  Serial.begin(9600);  
+  MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+  MAP_TimerConfigure(TIMER1_BASE, TIMER_CFG_PERIODIC);
+  MAP_TimerLoadSet(TIMER1_BASE, TIMER_A, MAP_SysCtlClockGet() / 100000);
+  TimerIntRegister(TIMER1_BASE, TIMER_A, &Timer1IntHandler);
+  MAP_TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+  MAP_TimerEnable(TIMER1_BASE, TIMER_A);
+
+  //Poner pines del 31 a 38 como salidas digitales.
+  for (int thisPin =lowestBit; thisPin <= highestBit; thisPin++) { 
+    pinMode(thisPin, OUTPUT); 
+  }
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly: 
-  Kp = 1;
-  Ki = 1;
-  Kd = 1;
-
+void loop()
+{
+  //Entrada
   Signal_Ind = analogRead(SensorH);
   Signal_Mag = analogRead(SensorL);
   Vref = analogRead(Ref);
 
-  e = Vref - (Signal_Mag - Signal_Ind);
-  ed = e - e_old;
-  E = E_old + e;
-  Actual_Value = Kp*e + Ki*E + Kd*ed;
-  e_old = e;
-  E_old = E;
+  //PID
+  erroractual = Vref - (Signal_Mag - Signal_Ind);
+  errorderivativo = (erroractual-errorviejo) /(MAP_SysCtlClockGet() / 100000) ;
+  Erroractual = (Errorviejo + erroractual)*(MAP_SysCtlClockGet() / 100000) ;
+  errortotal = (erroractual*Kp + Erroractual*Ki + errorderivativo*Kd) * 4095/255;
 
-  
-  
-  
+  //Salida
+  DACSignal = byte(controlVoltage);
+  DAC();
 }
+
+
+void Timer1IntHandler() {
+  MAP_TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+  Pot = analogRead(Ref);
+  sensorValue = analogRead(SensorH);
+  sensorValue1 = analogRead(SensorL);
+  Sensores = sensorValue-sensorValue1;
+  errorviejo = erroractual;
+  Errorviejo = Erroractual;
+  erroractual = Pot - Sensores;
+}
+
+void DAC(){
+    for (int index = 0; index <= 7; index++) { 
+      currentBit = bitRead(DACSignal, index);
+      if(currentBit == 0){
+        digitalWrite(index+31, LOW);
+      }
+      else{
+        digitalWrite(index+31, HIGH);
+        }
+    }
+}
+
+
+
+
