d81fabf0130030a1146f8b851e227fe491d72413
DanielGarciaGiron <daniel.garcia.giron@gmail.com>
2018-05-21 21:55:56 -0600

    Levitador Magnetico version 2.0

--- LevitadorMagnetico.ino
+++ LevitadorMagnetico.ino
@@ -1,11 +1,8 @@
-#include <stdint.h>
-#include <stdbool.h>
-#define PART_TM4C123GH6PM
-#include "inc/tm4c123gh6pm.h"
-#include "inc/hw_ints.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/timer.h"
+#include <inc/tm4c123gh6pm.h>
+//8000 PERIOD = 1 KILOHERTZ, can be modified.
+#define PERIOD 80000
+//PIN PF1 = RED_LED
+#define LED RED_LED
 
 
 //Analog Inputs, Potentiometer and Hall Effect sensors.
@@ -35,19 +32,22 @@ const int lowestBit = 31;
 const int highestBit = 39;
 int currentBit = 0;
 
-void initTimer()
-{  
-  ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-  ROM_TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);   // 32 bits Timer
-  TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0Isr);    // Registering  isr       
-  ROM_TimerEnable(TIMER0_BASE, TIMER_A); 
-  ROM_IntEnable(INT_TIMER0A); 
-  ROM_TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);  
+void initTimer(void)
+{
+  SYSCTL_RCGCTIMER_R |= 1;
+  TIMER0_CTL_R &= ~(1 << 8); // disable timer B
+  TIMER0_CFG_R  = 4; 
+  TIMER0_TBMR_R = 0b1010; // set T1AMS, T0MR=0
+  TIMER0_TBILR_R = PERIOD & 0xffff; // set interval (lower 16 bits) 
+  TIMER0_TBPR_R = PERIOD >> 16; // set interval (upper bits)
+  TIMER0_TBMATCHR_R = 100; //  set match value
+  TIMER0_CTL_R |= (1 << 8); // enable timer B
+  GPIO_PORTF_AFSEL_R |= (1 << 1); // select alternate function for PF1 
+  GPIO_PORTF_PCTL_R |= (7 << 4);
 }
 
-void Timer0Isr(void)
+void ISR(void)
 {
-  ROM_TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);  // Clear the timer interrupt
   DAC();
 }
 
@@ -58,29 +58,24 @@ void setup()
     pinMode(thisPin, OUTPUT); 
   }
   //Serial.begin(9600);
+  pinMode(LED, OUTPUT); 
   initTimer();
+  attachInterrupt(LED, ISR, RISING);
 }
 
 
 void loop()
 {
-  unsigned long ulPeriod;
-  unsigned int Hz = 100;   // frequency in Hz  
-  ulPeriod = (SysCtlClockGet() / Hz)/ 2;
-  ROM_TimerLoadSet(TIMER0_BASE, TIMER_A,ulPeriod -1);
-  while (1)
-  {
-    Signal_Ind = analogRead(sensorH);
-    Signal_Mag = analogRead(sensorL);
-    Vref = analogRead(ref);
+  Signal_Ind = analogRead(sensorH);
+  Signal_Mag = analogRead(sensorL);
+  Vref = analogRead(ref);
 
-    e = Vref - (Signal_Mag - Signal_Ind);
-    ed = e - e_old;
-    E = E_old + e;
-    Actual_Value = Kp*e + Ki*E + Kd*ed;
-    e_old = e;
-    E_old = E;
-  }
+  e = Vref - (Signal_Mag - Signal_Ind);
+  ed = e - e_old;
+  E = E_old + e;
+  Actual_Value = Kp*e + Ki*E + Kd*ed;
+  e_old = e;
+  E_old = E;
 }
 
 void DAC()
